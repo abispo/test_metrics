32e9c22ea49155f72f243b095fe0441c1cd5ce14
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2017-10-30 18:02:23 -0200

    Adding ipdf and ipython for debugging purposes. Modifying metrics models.

--- metrics/models.py
+++ metrics/models.py
@@ -4,6 +4,10 @@ class Video(models.Model):
     title = models.CharField(max_length=100, blank=False, default='Youtube Video')
     date_uploaded = models.DateTimeField()
     views = models.BigIntegerField()
+    theme = models.ManyToManyField('Theme', related_name='themes')
+
+    def __str__(self):
+        return self.title
 
 class VideoInteraction(models.Model):
     is_positive = models.BooleanField()
@@ -14,10 +18,16 @@ class VideoInteraction(models.Model):
         abstract = True
 
 class Comment(VideoInteraction):
-    pass
+    
+    def __str__(self):
+        return ("Comment {}".format(self.id))
 
 class Thumb(VideoInteraction):
-    pass
+    def __str__(self):
+        return ("Positive? {}".format(self.is_positive))
 
 class Theme(models.Model):
     name = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return self.name
